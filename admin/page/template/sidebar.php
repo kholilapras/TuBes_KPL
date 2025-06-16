ab25cd3725bd1f0ce38f35a578fabc809c12cacc
Kholil Abdi Prasetiyo <kholilapras@gmail.com>
2025-06-16 21:19:05 +0700

    update

--- admin/page/template/sidebar.php
+++ admin/page/template/sidebar.php
@@ -22,73 +22,47 @@ if (isset($_SESSION['role'])) {
 </head>
 
 <body>
-
-
-
     <aside class="sidebar">
         <ul class="sidebar-links">
-            <?php if ($role == 'Super Admin') : ?>
-                <h4>
-                    <span>CMS</span>
-                    <div class="menu-separator"></div>
-                </h4>
-                <li>
-                    <a href="page.php?page=info-web"><span class="material-symbols-outlined"> info </span>Info Web</a>
-                </li>
-                <li>
-                    <a href="page.php?page=beranda"><span class="material-symbols-outlined"> wallpaper_slideshow </span>Image Slider</a>
-                </li>
-                <li>
-                    <a href="page.php?page=banner-teks"><span class="material-symbols-outlined"> text_fields </span>Banner Teks</a>
-                </li>
-                <li>
-                    <a href="page.php?page=menu"><span class="material-symbols-outlined"> restaurant </span>Kelola Menu</a>
-                </li>
-                <li>
-                    <a href="page.php?page=kontak-masuk"><span class="material-symbols-outlined"> mail </span>Pesan Masuk</a>
-                </li>
-                <li>
-                    <a href="page.php?page=waktu-operasional"><span class="material-symbols-outlined"> event_available </span>Waktu Operasional</a>
-                </li>
-                <li>
-                    <a href="page.php?page=gallery"><span class="material-symbols-outlined"> image </span>Gallery</a>
-                </li>
-                <h4>
-                    <span>Kasir</span>
-                    <div class="menu-separator"></div>
-                </h4>
-            <?php endif; ?>
-
-            <?php if ($role == 'Super Admin' || $role == 'Kasir') : ?>
-                <li>
-                    <a href="page.php?page=daftar-menu"><span class="material-symbols-outlined"> brunch_dining </span>Menu Transaksi</a>
-                </li>
-                <li>
-                    <a href="page.php?page=transaksi"><span class="material-symbols-outlined"> receipt_long </span>Transaksi</a>
-                </li>
-                <li style="display:none;">
-                    <a href="page.php?page=laporan"><span class="material-symbols-outlined"> description </span>Laporan</a>
-                </li>
-            <?php endif; ?>
-
-            <?php if ($role == 'Super Admin' || $role == 'Kasir') : ?>
-                <h4>
-                    <span>Akun</span>
-                    <div class="menu-separator"></div>
-                </h4>
-                <li>
-                    <a href="page.php?page=users"><span class="material-symbols-outlined"> account_circle </span>Users</a>
-                </li>
-                <li>
-                    <a href="page.php?page=logout"><span class="material-symbols-outlined"> logout </span>Logout</a>
-                </li>
-            <?php endif; ?>
+            <h4>
+                <span>CMS</span>
+                <div class="menu-separator"></div>
+            </h4>
+            <li>
+                <a href="page.php?page=info-web"><span class="material-symbols-outlined"> info </span>Info Web</a>
+            </li>
+            <li>
+                <a href="page.php?page=beranda"><span class="material-symbols-outlined"> wallpaper_slideshow </span>Image Slider</a>
+            </li>
+            <li>
+                <a href="page.php?page=banner-teks"><span class="material-symbols-outlined"> text_fields </span>Banner Teks</a>
+            </li>
+            <li>
+                <a href="page.php?page=menu"><span class="material-symbols-outlined"> restaurant </span>Kelola Menu</a>
+            </li>
+            <li>
+                <a href="page.php?page=kontak-masuk"><span class="material-symbols-outlined"> mail </span>Pesan Masuk</a>
+            </li>
+            <li>
+                <a href="page.php?page=waktu-operasional"><span class="material-symbols-outlined"> event_available </span>Waktu Operasional</a>
+            </li>
+            <li>
+                <a href="page.php?page=gallery"><span class="material-symbols-outlined"> image </span>Gallery</a>
+            </li>
+            <h4>
+                <span>Akun</span>
+                <div class="menu-separator"></div>
+            </h4>
+            <li>
+                <a href="page.php?page=users"><span class="material-symbols-outlined"> account_circle </span>Users</a>
+            </li>
+            <li>
+                <a href="page.php?page=logout"><span class="material-symbols-outlined"> logout </span>Logout</a>
+            </li>
         </ul>
     </aside>
-
-
-
 </body>
+
 
 <style>
     @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap");
